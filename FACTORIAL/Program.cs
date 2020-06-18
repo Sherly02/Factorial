ab61e560b5c26ecec8b631058e03ec5f833ac45d
Sherly <sherly_mauren_25rpl@student.smktelkom-mlg.sch.id>
2020-06-18 08:21:59 +0700

    first commit

---
+++ FACTORIAL/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FACTORIAL
+{
+    class Program
+    {
+        long Factorial(long X)
+        {
+            long FX = 1;
+            if (X > 1) FX = Factorial(X - 1) * X;
+            return FX;
+        }
+
+        static void Main(string[] args)
+        {
+            long F;
+            Console.Write("\n n! = ");
+            F = long.Parse(Console.ReadLine());
+            Program n = new Program();
+
+            for (long R = 0; R <= F; R++)
+            { 
+                Console.WriteLine(" Factorial ke {0} = {1}",R,n.Factorial(R));
+            }
+            Console.ReadKey();
+        }
+    }
+}
